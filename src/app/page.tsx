491a2d61ca43fa67bee83232a9db4a44a24e52c8
= <=>
2024-02-28 13:45:21 +0700

    W03: Jawaban Soal 2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,13 @@
 import Image from "next/image";
 import Profile from "../components/profile";
 import "./globals.css";
+import { Gallery } from "@/components/gallery";
 
 export default function Home() {
   return (
     <main>
       <div>
-        <center>
-          <h1>Ilmuwan Luar Biasa</h1>
-        </center>
-      </div>
-      <div className="image-container">
-        <Profile /><Profile /><Profile />
+        <Gallery />
       </div>
     </main>
   );
