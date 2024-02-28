5f08147754125c327b5e47c69a671ab643d25f28
= <=>
2024-02-28 13:40:21 +0700

    W03 : Revisi Jawaban Soal 1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,17 @@
 import Image from "next/image";
 import Profile from "../components/profile";
+import "./globals.css";
 
 export default function Home() {
   return (
     <main>
       <div>
-        <center><h1>Ilmuwan yang Luar Biasa</h1></center>
-        <center><Profile /></center>
-        
+        <center>
+          <h1>Ilmuwan Luar Biasa</h1>
+        </center>
+      </div>
+      <div className="image-container">
+        <Profile /><Profile /><Profile />
       </div>
     </main>
   );
